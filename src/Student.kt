8beed0487d585768fdbc91c722740c607f4b610c
George Hampton <0hampg66@solent.ac.uk>
2025-10-02 16:37:49 +0100

    Added a get grade function

--- src/Student.kt
+++ src/Student.kt
@@ -1,4 +1,26 @@
-class Student (var id: String, var name: String, var course: String, var mark: Double) {
+class Student (var id: String, var name: String, var course: String, markIn: Double) {
+
+    var mark = 0.0
+        set(newMark) {
+            if (newMark in 0.0..100.0) {
+                field = newMark
+            }
+        }
+
+    init {
+        mark = markIn
+    }
+
+    fun get_grade(): String {
+        return when (mark) {
+            in 70.0..100.0 -> "First"
+            in 60.0..69.0 -> "2/1"
+            in 50.0..59.0 -> "2/2"
+            in 40.0..49.0 -> "Third"
+            in 0.0..39.0 -> "Fail"
+            else -> "Invalid input"
+        }
+    }
 
     override fun toString(): String {
         return "Name: $name, Course: $course, Mark: $mark"
